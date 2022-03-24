9d36983af2ce70e88a3cd8bc2923bf1f96930408
dlea8 <darrin.lea.2020@stpauls.com>
2022-03-24 00:43:08 -0500

    changed background image

--- src/Header/header.js
+++ src/Header/header.js
@@ -15,9 +15,9 @@ function Header() {
             </div>
             
             <div className="socials-flex">
-                <a href=""><FontAwesomeIcon className="icons grow" icon={faInstagram} size="lg"/></a>
-                <a href="https://www.linkedin.com/in/darrin-lea-a029941b8/"><FontAwesomeIcon className="icons grow" icon={faLinkedin} size="lg"/></a>
-                <a href="https://github.com/dlea8"><FontAwesomeIcon className="icons grow" icon={faGithub} size="lg" /></a>
+                <a href="" target="_blank"><FontAwesomeIcon className="icons grow" icon={faInstagram} size="lg"/></a>
+                <a href="https://www.linkedin.com/in/darrin-lea-a029941b8/" target="_blank"><FontAwesomeIcon className="icons grow" icon={faLinkedin} size="lg"/></a>
+                <a href="https://github.com/dlea8" target="_blank"><FontAwesomeIcon className="icons grow" icon={faGithub} size="lg" /></a>
             </div>
           </div>
           <div></div>
