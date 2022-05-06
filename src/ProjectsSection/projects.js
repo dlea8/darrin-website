00aaa50e9ea794d0cc48bae42b566687d47aee83
Darrin Lea <68883416+dlea8@users.noreply.github.com>
2022-05-06 17:41:22 -0500

    Feature/mobile (#1)
    * changed amx widths
    
    * cahnged name and started image formatting
    
    * fixed hover on project cards

--- src/ProjectsSection/projects.js
+++ src/ProjectsSection/projects.js
@@ -15,21 +15,28 @@ function Projects() {
             <label className="projects-section-title">Personal Projects</label>
         </div>
         <div className="projects-content">
-            <ProjectCard
-            title = "Sudoku Solver"
-            text = "Retreives a random Sudoku puzzle from an online dataset and solves the puzzle. Created in Summer of 2020 and implemented in Java.View more details and run it for yourself on my GitHub page"
-            image={sudoku} 
-            delay="0"/>
-            <ProjectCard
-            title = "Golf Assistant App"
-            text = "An Android app that helps you calculate your golf handicap and keep track of scores. Built in Android studio. Click to see features the app offers and view the app on GitHub."
-            image={golf}
-            delay="150"/>
-            <ProjectCard
-            title = "Personal Website"
-            text = "After teaching myself the ReactJs framework, I built this website from scratch using React. "
-            image={reactLogo}
-            delay="300"/>
+            <div className="project-grow">
+              <ProjectCard
+              title = "Sudoku Solver"
+              text = "Retreives a random Sudoku puzzle from an online dataset and solves the puzzle. Created in Summer of 2020 and implemented in Java.View more details and run it for yourself on my GitHub page"
+              image={sudoku} 
+              delay="0"/>
+            </div>
+            <siv className="project-grow ">
+              <ProjectCard
+              title = "Golf Assistant App"
+              text = "An Android app that helps you calculate your golf handicap and keep track of scores. Built in Android studio. Click to see features the app offers and view the app on GitHub."
+              image={golf}
+              delay="150" />
+            </siv>
+            <div className="project-grow ">
+              <ProjectCard
+              title = "Personal Website"
+              text = "After teaching myself the ReactJs framework, I built this website from scratch using React. "
+              image={reactLogo}
+              delay="300" />
+            </div>
+           
         </div>
     </div>
   );
--- src/WelcomeSection/welcome.js
+++ src/WelcomeSection/welcome.js
@@ -3,6 +3,7 @@ import './welcome.css';
 import React, {useEffect, useState} from "react";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import image from "../assets/headshot.jpg";
 
 function Welcome() {
 
@@ -14,11 +15,12 @@ function Welcome() {
 
     return (
       <div className="welcome-section">
-          <div className='welcome-header' data-aos="fade">
-            <div className='welcome-wrapper'><label className="welcome-lbl">Welcome!</label></div>
-               
+        <div className='text-div'>
+          <div className='welcome-header' >
+              <div className='welcome-wrapper'><label className="welcome-lbl">Welcome!</label></div>
+                
           </div>
-          <div className="welcome-msg" data-aos="fade">
+          <div className="welcome-msg" >
               <p>My name is Darrin Lea and I’m a Sophomore at Louisiana State University 
                   majoring in Computer Science and Math. 
                   I’ve always had a fascination with computer and a thirst for learning. 
@@ -28,6 +30,10 @@ function Welcome() {
                   Be sure to check out some of my personal projects and feel free to reach out 
                   via LinkedIn or email!</p>
           </div>
+        </div>
+        <div className='img-div'>
+          <img src={image} className='welcome-img-style'></img>
+        </div>
       </div>
     );
   }
