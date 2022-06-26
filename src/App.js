7eff2a9f6051f17c363a91aca7134a84dcccd7f1
dlea8 <darrin.lea.2020@stpauls.com>
2022-06-25 20:29:17 -0500

    Formatting on sudoku page

--- src/App.js
+++ src/App.js
@@ -5,17 +5,28 @@ import Welcome from './WelcomeSection/welcome';
 import Projects from './ProjectsSection/projects';
 import NavBar from './NavBar/navbar';
 import Footer from './Footer/footer';
+import SudokuSolver from './Pages/SudokuSolver';
+import Home from "./Pages/Home";
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <NavBar></NavBar>
-      <Header></Header>
-      <Welcome></Welcome>
-      <Projects/>
-      <Footer></Footer>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/SudokuSolver" element={<SudokuSolver/>}/>
+        </Routes>
+      </div>
+    </Router>
+    // <div className="App">
+    //   <NavBar></NavBar>
+    //   <Header></Header>
+    //   <Welcome></Welcome>
+    //   <Projects/>
+    //   <Footer></Footer>
 
-    </div>
+    // </div>
   );
 }
 
--- src/Footer/footer.js
+++ src/Footer/footer.js
@@ -21,10 +21,12 @@ function Footer() {
         <div className="footer-name" onClick={e => scrollToSection("header")}>
             <label>Darrin Lea</label>
         </div>
-        <div className="footer-socials">
-            <a href="mailto:dlea1@lsu.edu" target="_blank"><FontAwesomeIcon className="footer-icons grow" icon={faEnvelope} size="lg"/></a>
-            <a href="https://www.linkedin.com/in/darrin-lea-a029941b8/" target="_blank"><FontAwesomeIcon className="footer-icons grow" icon={faLinkedin} size="lg"/></a>
-            <a href="https://github.com/dlea8" target="_blank"><FontAwesomeIcon className="footer-icons grow" icon={faGithub} size="lg" /></a>
+        <div >
+          <div className="footer-socials">
+              <div><a href="mailto:dlea1@lsu.edu" target="_blank"><FontAwesomeIcon className="footer-icons grow" icon={faEnvelope} size="lg"/></a></div>
+              <div><a href="https://www.linkedin.com/in/darrin-lea-a029941b8/" target="_blank"><FontAwesomeIcon className="footer-icons grow" icon={faLinkedin} size="lg"/></a></div>
+              <div><a href="https://github.com/dlea8" target="_blank"><FontAwesomeIcon className="footer-icons grow" icon={faGithub} size="lg" /></a></div>
+          </div>
         </div>
         <div className="copywrite">
             Copywrite © {new Date().getFullYear()} Darrin Lea
