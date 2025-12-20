1d323c8f3f057e25656559d9bb79316faaca0417
Darrin <darrinlea@gmail.com>
2025-12-20 12:27:20 -0700

    change background image

--- src/NavBar/navbar.js
+++ src/NavBar/navbar.js
@@ -12,7 +12,6 @@ function NavBar() {
             <div onClick={e => scrollToSection("welcome-section")}><label className="nav-item">About</label></div>
             <div onClick={e => scrollToSection("projects-section")}><label className="nav-item">Projects</label></div>
             <div><a className="resume-link" href="https://darrinfiles.s3.us-east-2.amazonaws.com/Darrin+Lea+Resume+September+2022.pdf" target="None"><label className="nav-item">Resume</label></a></div>   
-            {/* link to resume somehow */}
         </div>
 
     </div>
