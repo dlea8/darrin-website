ce74432acc1046018339a21ea22928a9f5c5fcd4
Darrin <darrinlea@gmail.com>
2025-12-30 16:23:48 -0600

    added link to publication card

--- src/PublicationsSection/publicationCard.js
+++ src/PublicationsSection/publicationCard.js
@@ -1,27 +1,26 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./publications.css"
-import { faExternalLink } from "@fortawesome/free-solid-svg-icons";
 
 export default function PublicationCard({
     title,
     authors,
     date,
     image, 
-    backgroundColor = "#ffffff"
+    backgroundColor = "#ffffff",
+    link
 }) {
 
     return (
-        <div className="publication-card-container card" style={{ '--image-bg-color': backgroundColor }}>
-            <div className="publications-text-container">
-                <label className="card-title">{title}</label>
-                <label className="card-subtitle">Published: {date}</label>
-                <label className="card-subtitle">Authors: {authors}</label>
+        <a href={link} className="publication-card-link" target="_blank" rel="noopener noreferrer">
+            <div className="publication-card-container card" style={{ '--image-bg-color': backgroundColor }}>
+                <div className="publications-text-container">
+                    <label className="card-title">{title}</label>
+                    <label className="card-subtitle">Published: {date}</label>
+                    <label className="card-subtitle">Authors: {authors}</label>
+                </div>
+                <div className="publication-image-container">
+                    <img src={image} alt="Unavailable"/>
+                </div>
             </div>
-            {/* <FontAwesomeIcon className="publication-link-icon" icon={faExternalLink} size="lg"/> */}
-            {/* some kind of link to the pbulication as well */}
-            <div className="publication-image-container">
-                <img src={image} alt="Unavailable"/>
-            </div>
-        </div>
+        </a>
     );
 }
