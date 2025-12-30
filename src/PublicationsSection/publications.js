7bcc04be444f5084c0c9300f78bc0bdead4bb8ca
Darrin <darrinlea@gmail.com>
2025-12-30 10:41:06 -0600

    Mad epublication card mobile responsive

--- src/PublicationsSection/publications.js
+++ src/PublicationsSection/publications.js
@@ -5,6 +5,7 @@ import PublicationCard from "./publicationCard.js";
 import lsu_icon from '../assets/lsu_icon.png';
 import inl_logo from "../assets/inl_logo.png";
 import tmobile_logo from "../assets/tmobile_logo.png"
+import acsac_icon from "../assets/acsac_logo.png"
 
 
 export default function Publications() {
@@ -14,8 +15,9 @@ export default function Publications() {
                 <label>Publications</label>
             </div>
             <div className="section-content-container publications-content">
+                <PublicationCard image={acsac_icon} backgroundColor="#0b843e60"/>
                 <PublicationCard image={lsu_icon} backgroundColor="#4c258160"/>
-                <PublicationCard image={inl_logo} backgroundColor="#8ec23f60"/>
+                <PublicationCard image={inl_logo} backgroundColor="#2ba2d960"/>
                 <PublicationCard image={tmobile_logo} backgroundColor="#e5017e60"/>
             </div>
         </div>
