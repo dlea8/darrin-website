46cadf19513665c3a75f02c296ed8312915e0a06
Darrin <darrinlea@gmail.com>
2025-12-30 11:03:29 -0600

    added publication info

--- src/PublicationsSection/publicationCard.js
+++ src/PublicationsSection/publicationCard.js
@@ -3,17 +3,19 @@ import "./publications.css"
 import { faExternalLink } from "@fortawesome/free-solid-svg-icons";
 
 export default function PublicationCard({
+    title,
+    authors,
+    date,
     image, 
     backgroundColor = "#ffffff"
 }) {
 
-    console.log(image);
     return (
         <div className="publication-card-container card">
             <div className="publications-text-container">
-                <label className="card-title">Publicaiton Title</label>
-                <label className="card-subtitle">1/21/2025</label>
-                <label className="card-subtitle">By: Darrin Lea + other authors if applicable</label>
+                <label className="card-title">{title}</label>
+                <label className="card-subtitle">Published: {date}</label>
+                <label className="card-subtitle">Authors: {authors}</label>
             </div>
             {/* <FontAwesomeIcon className="publication-link-icon" icon={faExternalLink} size="lg"/> */}
             {/* some kind of link to the pbulication as well */}
--- src/PublicationsSection/publications.js
+++ src/PublicationsSection/publications.js
@@ -15,10 +15,41 @@ export default function Publications() {
                 <label>Publications</label>
             </div>
             <div className="section-content-container publications-content">
-                <PublicationCard image={acsac_icon} backgroundColor="#0b843e60"/>
-                <PublicationCard image={lsu_icon} backgroundColor="#4c258160"/>
-                <PublicationCard image={inl_logo} backgroundColor="#2ba2d960"/>
-                <PublicationCard image={tmobile_logo} backgroundColor="#e5017e60"/>
+                <PublicationCard 
+                    title="REx86: A Local Large Language Model for Assisting in x86 Assembly Reverse Engineering" 
+                    date="12/10/2025"
+                    authors="Darrin Lea, James Ghawaly, Golden Richard III, Aisha Ali-Gombe, Andrew Case"
+                    image={acsac_icon} 
+                    backgroundColor="#0b843e60"
+                    link="https://arxiv.org/abs/2510.20975"/>
+                <PublicationCard
+                    title="Optimizing LLM x86 Assembly Code Comprehension through Fine-Tuning"
+                    date="4/3/2025"
+                    authors="Darrin Lea"
+                    image={lsu_icon} 
+                    backgroundColor="#4c258160"
+                    link="https://repository.lsu.edu/gradschool_theses/6140/"/>
+                <PublicationCard
+                    title="Reducing AI RAG Hallucination by Optimizing Routing Techniques"
+                    date="8/16/2024"
+                    authors="Darrin Lea, Rafer Cooley, Michael Cutshaw, Zachary Priest"
+                    image={inl_logo} 
+                    backgroundColor="#2ba2d960"
+                    link="https://www.osti.gov/biblio/2474834"/>
+                <PublicationCard
+                    title="Threat Modeling Industrial Control Systems"
+                    date="4/2024"
+                    authors="Darrin Lea"
+                    image={lsu_icon} 
+                    backgroundColor="#4c258160"
+                    link=""/>
+                <PublicationCard
+                    title="Rescue System (US12302448B2)"
+                    date="5/13/2025"
+                    authors="Jim Song, Sarah Lynn O'Brien, Darrin Michael Lea, Henry Vy, Paulyn Ladignon Monasterio, Eric Steven Hill"
+                    image={tmobile_logo}
+                    backgroundColor="#e5017e60"
+                    link="https://patents.google.com/patent/US12302448B2/en?oq=US12302448B2"/>
             </div>
         </div>
     );
